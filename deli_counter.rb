b2d36914dd39676bb279115824a191c4c268edc2
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-01-29 03:04:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ end
 
 def take_a_number(katz_deli, name)
   #binding.pry
-  if katz_deli.empty?
+  katz_deli.empty?
     katz_deli << name
   katz_deli.each_with_index do |name, index|
   puts "Welcome, #{name}. You are number #{index + 1} in line."
