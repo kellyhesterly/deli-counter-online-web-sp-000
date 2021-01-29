3b24d5f2820ac4c9827537e5ae3f11c8713bb2b3
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-01-29 03:15:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,9 @@ end
 
 
 def take_a_number(katz_deli, name)
-  #binding.pry
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving
 end
